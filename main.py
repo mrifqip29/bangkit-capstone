309566e412b66b085a53e3a78d3b19ba17be4d3a
Muhammad Rifqi Prabantoro <45098478+mrifqip29@users.noreply.github.com>
2021-06-03 18:57:59 +0700

    init commit

--- main.py
+++ main.py
@@ -1,30 +1,102 @@
-import json
-import joblib
-
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.models import Sequential, load_model
 from flask import Flask, request
+from PIL import Image
+import flask
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+import io
 
+# from keras.models import load_model
+
+# instantiate flask
 app = Flask(__name__)
 
-iris_decision_tree = joblib.load("iris_decision_tree.joblib")
+#  load model, and pass in the custom metric
+global graph
+graph = tf.compat.v1.get_default_graph()
+model = load_model('traditional_cake.h5')
+
+
+def prepare_image(image, target_size):
+    # resize the input image and preprocess it
+    image = image.resize(target_size)
+    image = img_to_array(image)
+    image = np.expand_dims(image, axis=0)
+    image = np.vstack([image])
+
+    # return the processed image
+    return image
+
+
+class_names = ['kue_dadar_gulung', 'kue_kastengel', 'kue_klepon',
+               'kue_lapis', 'kue_lumpur', 'kue_putri_salju', 'kue_risoles', 'kue_serabi']
+
 
 @app.route("/")
 def hello():
-    return "Hello, World!"
+    return "Hello, Bangkit!"
+
 
 @app.route("/predict", methods=["POST"])
 def predict():
-    request_json = request.json
-    print("data: {}".format(request_json))
-    print("type: {}".format(type(request_json)))
+    data = {"success": False}
 
-    prediction = iris_decision_tree.predict(request_json.get('data'))
-    prediction_string = [str(d) for d in prediction]
-    response_json = {
-        "data" : request_json.get("data"),
-        "prediction" : list(prediction_string)
-    }
+    if flask.request.method == "POST":
+        if flask.request.files.get("image"):
+            # indicate that the request was a success
+            data["success"] = True
 
-    return json.dumps(response_json)
+            # read the image in PIL format
+            image = flask.request.files["image"].read()
+            image = Image.open(io.BytesIO(image))
+
+            # preprocess the image and prepare it for classification
+            resizedImage = prepare_image(image, target_size=(150, 150))
+
+            classes = model.predict(resizedImage, batch_size=10)
+
+            x = np.where(classes[0] == 1)[0][0]
+
+            data["prediction"] = class_names[x]
+
+    print("data: {}".format(data))
+    print("data_type: {}".format(type(data)))
+
+    return flask.jsonify(data)
+
+
+@app.route("/predict_test", methods=["POST"])
+def predict():
+    data = {"success": False}
+
+    if flask.request.method == "POST":
+        if flask.request.files.get("image"):
+            # indicate that the request was a success
+            data["success"] = True
+
+            data["image"] = image
+
+            # read the image in PIL format
+            image = flask.request.files["image"].read()
+            image = Image.open(io.BytesIO(image))
+
+            # preprocess the image and prepare it for classification
+            resizedImage = prepare_image(image, target_size=(150, 150))
+
+            classes = model.predict(resizedImage, batch_size=10)
+
+            x = np.where(classes[0] == 1)[0][0]
+
+            data["prediction"] = class_names[x]
+
+    print("data: {}".format(data))
+    print("data_type: {}".format(type(data)))
+
+    return flask.jsonify(data)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
